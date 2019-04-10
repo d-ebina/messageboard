e268271406d284ae67974e19d9ed8be15f91cb05
d-ebina <ebina@sugarcorp.jp>
2019-04-10 12:13:06 +0900

    add: messageboard

---
+++ messageboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  messageboard
+//
+//  Created by SUGAR-PC02 on 2019/04/10.
+//  Copyright © 2019 SUGAR-PC02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
